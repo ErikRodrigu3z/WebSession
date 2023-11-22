9399f3a65e465a431d7b62fca8ca100951b1b271
erik <erik@VCL>
2023-11-22 11:46:26 -0700

    Agregar archivos de proyecto.

---
+++ WebTest/Controllers/CustomAuthorizeAttribute.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.Web;
+using System.Web.Mvc;
+using WebTest.Models;
+
+namespace WebTest.Controllers
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {          
+            bool _isAuthorized = false;
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            
+            if (httpContext.User.Identity.IsAuthenticated)
+            {
+                var user = userManager.FindByName(httpContext.User.Identity.Name);  
+                
+                //Validate user and rol
+                if (!string.IsNullOrEmpty(base.Roles))
+                {
+                    foreach (var rol in base.Roles.Split(','))
+                    {
+                        // Verifica si la cookie de sesión almacenada coincide con la actual
+                        var storedSessionId = httpContext.Request.Cookies["MyAppSessionId"]?.Value;
+
+                        if (userManager.IsInRole(userManager.FindByName(httpContext.User.Identity.Name).Id, rol) && 
+                            user.SessionID == storedSessionId)
+                        {
+                            _isAuthorized = true;
+                        }                        
+                    }
+                }
+                else //if _roles is null, the end point only vaidate if user Is Authenticated and SessionID is the same
+                {
+                    return user.SessionID == httpContext.Session.SessionID;
+                }               
+            }
+            return _isAuthorized;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            // Manejo personalizado para solicitudes no autorizadas, si es necesario.
+            // Por ejemplo, redirige a una página de acceso denegado.
+
+            filterContext.Result = new HttpUnauthorizedResult("Acceso denegado. Mensaje personalizado.");
+        }
+
+
+
+    }
+}
